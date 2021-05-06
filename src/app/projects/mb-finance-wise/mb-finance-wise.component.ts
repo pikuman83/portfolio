03c88b444713590e1b7e7667187992210ab422e7
Pikuman83 <wahab_anjum@hotmail.com>
2021-05-06 23:37:17 +0200

    'mbfw-project'

--- src/app/projects/mb-finance-wise/mb-finance-wise.component.ts
+++ src/app/projects/mb-finance-wise/mb-finance-wise.component.ts
@@ -10,6 +10,22 @@ export class MbFinanceWiseComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.respWidth();
   }
 
+  width: number = 800;
+
+  respWidth(){
+    if (window.innerWidth <= 850) {
+      this.width = 350;
+    }
+  }
+  respWidthP(event: any){
+    if (event.target.innerWidth <= 850){
+      this.width = 350;
+    }
+    else{
+      this.width = 800;
+    }
+  }
 }
