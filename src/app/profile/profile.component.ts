e94bb53653f6124809bdbcbc10b6f3a48fe930c9
Pikuman83 <wahab_anjum@hotmail.com>
2021-05-11 20:15:01 +0200

    'responsive-images'

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -10,6 +10,42 @@ export class ProfileComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.respWidth();
+  }
+
+  width = 168;
+  height = 168;
+
+  respWidth(){
+    const x = window.innerWidth;
+    const y = x - ((x*20)/100);
+    const i = document.getElementsByName('fotoFrame');
+    if (x < 767) {
+      this.width = 300;
+      this.height = 200;
+      for (let q = 0; q<= i.length-1; q++){
+        document.getElementsByName('fotoFrame')[q].classList.remove('card');
+      }
+    }
+  }
+  respWidthP(event: any){
+    const x = event.target.innerWidth;
+    const y = x - ((x*20)/100);
+    const i = document.getElementsByName('fotoFrame');
+    if (x < 767){
+      this.width = 300;
+      this.height = 200;
+      for (let q = 0; q<= i.length; q++){
+        document.getElementsByName('fotoFrame')[q].classList.remove('card');
+      }
+    }
+    else{
+      this.width = 168;
+      this.height = 168;
+      for (let q = 0; q<= i.length; q++){
+        document.getElementsByName('fotoFrame')[q].classList.add('card');
+      }
+    }
   }
 
 }
